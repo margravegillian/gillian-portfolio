0ea83bf2564264a93d0ac8703e6afd40dcb40eaf
margravegillian <urvaius@gmail.com>
2024-04-29 08:11:08 -0700

    added good favicon

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -5,13 +5,16 @@
         {name: 'Projects', link: '#projects'},
         {name: 'About Me', link: '#about'},
         //{name: 'Contact', link: 'mailto:margravegillian@gmail.com'}
+        
     
     ];
+   
 </script>
 
 <header class={'sticky z-[10] top-0 duraton-200 flex items-center justify-between border border-solid ' + (
     y > 0 ? ' py-4 bg-slate-950 border-pink-800' : ' py-6 bg-transparent border-transparent'
 )}>
+ 
     <h1 class="font-medium">
         <b class="font-bold poppins">Gillian</b> Arneson
     </h1>
